e75a57b485f44cf84b3ac87ea18f589a63b824b1
Artem Kopshik <artemktk@gmail.com>
2023-07-25 19:08:23 +0300

    Первый метод

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,13 @@
 ﻿string [] arr1 = {"Hello", "2", "world", ":-)"};
 string [] arr2 = {"1234", "1567", "-2", "computer science"};
 string [] arr3 = {"Russia", "Denmark", "Kazan"};
+
+int CalcFinishArray(string [] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3) count++;
+    }
+    return count;
+}
+ 
